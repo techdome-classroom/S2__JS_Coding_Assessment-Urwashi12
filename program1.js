eae4fc9826bd59a7af6dd9fb51db2d4763b60070
Urwashi12 <urwashiranjan1010@gmail.com>
2024-09-25 15:17:43 +0530

    I Had Done.

--- program1.js
+++ program1.js
@@ -3,6 +3,24 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+      const stack = [];
+    const matchingBrackets = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    };
+
+    for (let char of s) {
+        if (['(', '[', '{'].includes(char)) {
+            stack.push(char);
+        } else if ([')', ']', '}'].includes(char)) {
+            if (stack.length === 0 || stack.pop() !== matchingBrackets[char]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
     
 };
 
--- program2.js
+++ program2.js
@@ -3,6 +3,34 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+      const roman = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        };
+    
+        let result = 0;
+    
+        for (let i = 0; i < s.length; i++) {
+            
+            const currentValue = roman[s[i]];
+            
+            
+            const nextValue = (i < s.length - 1) ? roman[s[i + 1]] : 0;
+    
+            
+            if (currentValue < nextValue) {
+                result -= currentValue; 
+            } else {
+                result += currentValue;
+            }
+        }
+    
+        return result; 
     
 };
 
